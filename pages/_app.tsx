4fab1ddb79c110fdf8d3a0d29e64211da185489e
Ivan Reshetnikov <ordinarydev@protonmail.com>
2022-10-09 23:59:24 +0500

    Add main page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,12 @@
-import '../styles/globals.css'
+import "@fontsource/roboto/300.css"
+import "@fontsource/roboto/400.css"
+import "@fontsource/roboto/700.css"
+import "@fontsource/roboto/900.css"
 import type { AppProps } from 'next/app'
+import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }: AppProps) {
+function AltWeb({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default AltWeb
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,39 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { FormEvent } from 'react'
+import { MailOutlined, RightOutlined } from '@ant-design/icons'
+import styles from '../styles/index.module.css'
+import Photos from '../lib/photos'
 
 const Home: NextPage = () => {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-      </Head>
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Alt | Web</title>
+            </Head>
+            <Photos />
+        <div className={styles.title}><span className={styles.alt}>Alt</span> Web</div>
+        <div className={styles.slogan}>A new era of web development</div>
+        <div className={styles.formContainer}>
+            <div className={styles.description}>Create a free account or sign in to an existing one</div>
+            <form onSubmit={(e) => submitForm(e)}>
+                <MailOutlined />
+                <input placeholder="Email" type="email" name="email" required/>
+                <button><RightOutlined /></button>
+            </form>
+        </div>
     </div>
   )
+}
+
+interface EmailForm extends HTMLFormElement {
+    email: HTMLInputElement
+}
+
+const submitForm = async(e: FormEvent) => {
+    e.preventDefault()
+    const target = e.target as EmailForm
+    target.reset()
 }
 
 export default Home
