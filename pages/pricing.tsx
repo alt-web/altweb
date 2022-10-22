2d60dd3d40d42a0437d9245c01ad4248edf2ce4f
Ivan Reshetnikov <ordinarydev@protonmail.com>
2022-10-22 15:40:16 +0500

    Add the page with prices

---
+++ pages/pricing.tsx
@@ -0,0 +1,31 @@
+import Head from "next/head"
+import type { NextPage } from "next"
+import Header from "../lib/header"
+import BG from "../lib/bg"
+import Service from "../lib/service"
+
+const Pricing: NextPage = () => {
+    return (
+        <div>
+            <Head><title>Цены - Alt Web</title></Head>
+            <Header />
+            <BG />
+            <Service name="Разработка сайта"
+                     description="Вы получите полностью готовый проект, с которым сможете делать всё, что душе угодно"
+                     price={40000} />
+            <Service name="Хостинг"
+                     description="Разместим ваш сайт в интернете, настроим почту"
+                     price={1000}
+                     perMonth={true} />
+            <Service name="Поддержка проекта"
+                     description="Устранение критических багов и уязвимостей, незначительные изменения"
+                     price={0} />
+            <Service name="Open-source"
+                     description="Ваше разрешение на размещение кода в открытом доступе позволит вам получить скидку, а нам
+                                  будет приятно"
+                     discount={-10} />
+        </div>
+    )
+}
+
+export default Pricing
