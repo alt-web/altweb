7e297a59f47ba3cd7cff0c89746e022ac3a3125a
Ivan Reshetnikov <ordinarydev@protonmail.com>
2022-10-25 22:11:56 +0500

    Make the cards look the same

--- lib/paper/index.tsx
+++ lib/paper/index.tsx
@@ -1,12 +1,10 @@
 import { ReactNode } from "react"
 import styles from "./paper.module.css"
 
-const Paper = (props: {children: ReactNode}) => {
+const Paper = (props: { children: ReactNode }) => {
     return (
         <div className={styles.container}>
-            <div className={styles.paper}>
-                {props.children}
-            </div>
+            <div className={styles.paper}>{props.children}</div>
         </div>
     )
 }
--- lib/service/index.tsx
+++ lib/service/index.tsx
@@ -1,34 +1,45 @@
+import Paper from "../paper"
 import styles from "./service.module.css"
 
-const Service = (props: {name: string, description: string, price?: number, perMonth?: boolean, discount?: number}) => {
+const Service = (props: {
+    name: string
+    description: string
+    price?: number
+    perMonth?: boolean
+    discount?: number
+}) => {
     return (
-        <div className={styles.container}>
-            <div className={styles.content}>
-                <div className={styles.row}>
-                    <h2>{props.name.toUpperCase()}</h2>
-                    <Value price={props.price} perMonth={props.perMonth} discount={props.discount} />
-                </div>
-                <div>
-                    {props.description}
-                </div>
+        <Paper>
+            <div className={styles.row}>
+                <h2>{props.name.toUpperCase()}</h2>
+                <Value
+                    price={props.price}
+                    perMonth={props.perMonth}
+                    discount={props.discount}
+                />
             </div>
-        </div>
+            <div>{props.description}</div>
+        </Paper>
     )
 }
 
-const Value = (props: {price?: number, perMonth?: boolean, discount?: number}) => {
+const Value = (props: {
+    price?: number
+    perMonth?: boolean
+    discount?: number
+}) => {
     if (props.price !== undefined) {
         let price = props.price.toLocaleString("ru") + " ₽"
         if (props.perMonth) price += " / мес"
         if (props.price > 0) return <h2>{price}</h2>
         return <h2 className={styles.greatOffer}>{price}</h2>
     }
-    
+
     if (props.discount !== undefined) {
         let discount = props.discount + " %"
         return <h2 className={styles.greatOffer}>{discount}</h2>
     }
-    
+
     return <></>
 }
 
