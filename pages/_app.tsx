e9c11ca28583068248a06bae445737f90bf5d700
Ivan Reshetnikov <ordinarydev@protonmail.com>
2022-10-30 14:45:37 +0500

    Update favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import "@fontsource/roboto/400.css"
 import "@fontsource/roboto/700.css"
 import "@fontsource/roboto/900.css"
 import { SWRConfig } from "swr"
+import Head from "next/head"
 import type { AppProps } from "next/app"
 import Background from "../lib/bg"
 import Header from "../lib/header"
@@ -16,11 +17,24 @@ function AltWeb({ Component, pageProps }: AppProps) {
                 fetcher: (resource, init) =>
                     fetch(resource, init).then(res => res.json()),
             }}>
+            <Favicons />
             <Background />
             <Header />
             <Component {...pageProps} />
         </SWRConfig>
     )
 }
+
+const Favicons = () => (
+    <Head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+    </Head>
+)
 
 export default AltWeb
