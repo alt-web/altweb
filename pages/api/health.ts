083ba4195c198cf6c909c90291fd7fc4d2ca7ce0
Ivan Reshetnikov <ordinarydev@protonmail.com>
2022-11-17 18:40:17 +0500

    Remove api and all related functions (#58)
    They are useless in static site

--- pages/api/health.ts
+++
@@ -1,7 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next"
-
-const handle = async (_req: NextApiRequest, res: NextApiResponse) => {
-    res.status(200).json({ ok: true })
-}
-
-export default handle
