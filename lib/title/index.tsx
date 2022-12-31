235645b9ca858e699f02d14c537b01d049b57679
Ivan Reshetnikov <ordinarydev@protonmail.com>
2022-12-31 16:54:34 +0500

    Add random phrases to homepage (#89)

--- lib/title/index.tsx
+++ lib/title/index.tsx
@@ -1,12 +1,31 @@
+import { useState, useEffect } from "react"
 import styles from "./title.module.css"
 
+const phrases = [
+    "Мы поможем вам оказаться в интернете",
+    "Здесь делают интернет",
+    "Сеть и настоящий мир — одно и то же",
+]
+
 const Title = () => {
+    const [phrase, setPhrase] = useState("")
+
+    const selectRandomPhrase = () => {
+        let index = Math.floor(Math.random() * phrases.length)
+        if (phrases[index] === phrase) index += 1
+        if (index === phrases.length) index = 0
+        const randomPhrase = phrases[index]
+        setPhrase(randomPhrase)
+    }
+
+    useEffect(selectRandomPhrase, [])
+
     return (
         <div className={styles.container}>
             <h1>Alt Web</h1>
-            <div className={styles.description}>
-                Мы поможем вам оказаться в интернете
-            </div>
+            <p className={styles.description} onClick={selectRandomPhrase}>
+                {phrase}
+            </p>
         </div>
     )
 }
